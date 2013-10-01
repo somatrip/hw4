d648acd1d86962be38841980341024a2499f55dd
Max Fridman <mnf234@nyu.edu>
2013-10-01 01:49:57 -0400

    hw4 1 49 am build

---
+++ hw4 git/day7/Assets/scripts/fish.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fish : MonoBehaviour {
+	
+	public Vector3 destination = new Vector3 (100f, 100f, 100f);
+	public float speed = 5f;
+
+	// Use this for initialization
+	void Start () {
+		
+		//keep repeating ____ function, after delay of 0 s, repeat every 15 seconds
+		InvokeRepeating("SetNewDestination", 0f, 3.5f);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.LookRotation ( rigidbody.velocity * Time.deltaTime );
+	}
+	
+	void FixedUpdate() {
+		//rigidbody.AddForce( destination - transform.position );
+		Vector3 direction = Vector3.Normalize( destination - transform.position);
+		rigidbody.AddForce (direction * speed, ForceMode.Acceleration);	
+		
+	}
+	
+	public void SetNewDestination() {
+		//set a random destination
+		SetNewDestination ( Random.insideUnitSphere * 50f );
+		
+	}
+	
+	 public void SetNewDestination (Vector3 newDestination) {
+        destination = newDestination;
+    }
+		
+	
+}
---
+++ hw4 git/day7/Assets/scripts/fishgod.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic; //You need this line to use lists.
+
+public class fishgod : MonoBehaviour {
+	
+	public fish fishBlueprint;
+	public fish fishBlueprint2;
+	public int fishCount = 100;
+	
+	public List<fish> fishList = new List<fish>();
+
+	// Use this for initialization
+	void Start () {
+		int currentFishCounter = 0;
+		while ( currentFishCounter < fishCount ) {
+			var randomNumber = 10 * ( Random.value );
+				
+		if (randomNumber < 5) {
+				
+			//spawn a fish	
+			 Vector3 fishPosition = Random.insideUnitSphere * 100f;
+			fish newfish = Instantiate( fishBlueprint, fishPosition, Quaternion.identity) as fish;
+			fishList.Add (newfish);
+			
+			//increment our fish counter
+			currentFishCounter++;
+				
+			}		
+		else
+			{
+			//spawn a fish
+        	 Vector3 fishPosition = Random.insideUnitSphere * 100f;
+			fish newfish = Instantiate( fishBlueprint2, fishPosition, Quaternion.identity) as fish;
+			fishList.Add (newfish);
+			
+			//increment our fish counter
+			currentFishCounter++;
+		
+			}
+		}	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if ( Input.GetKeyDown( KeyCode.Space ) ) {
+			
+			foreach (fish currentfish in fishList ) {
+				currentfish.destination = Vector3.zero;
+				
+			}
+			
+		}
+	
+	}
+}
